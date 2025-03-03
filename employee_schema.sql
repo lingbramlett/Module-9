342edcef705f7fde0817ced896d94e6c371320d8
lingbramlett <lingbramlett@gmail.com>
2025-03-03 18:17:19 -0500

    Update employee_schema.sql

--- employee_schema.sql
+++ employee_schema.sql
@@ -1,6 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/P9jczF
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
 CREATE TABLE "Departments" (
